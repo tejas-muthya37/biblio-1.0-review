9079c3bbe346c7f6e96299b5a5f6651ca7768736
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-03-30 18:52:12 +0530

    Chore: Files not related to functionality

---
+++ src/Screens/Success/Success.js
@@ -0,0 +1,25 @@
+import "./success.css";
+import { useProducts } from "./../../Context/products-context.js";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Success() {
+  const { setCartArray } = useProducts();
+
+  useEffect(() => {
+    setCartArray([]);
+    localStorage.setItem("CART_ARRAY", JSON.stringify([]));
+  }, [setCartArray]);
+
+  return (
+    <div className="Success">
+      <span>✔</span>
+      <h1>Your order has been placed!</h1>
+      <Link to="/">
+        <button className="btn-continue-shopping">Continue Shopping</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Success;
